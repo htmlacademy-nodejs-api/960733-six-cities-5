22197975e98a894216c9018cef7cc73b32555e4b
Roman Kostenko <kostenko.r.khv@gmail.com>
2023-10-06 11:45:49 +1000

    3.9. Архитектура приложения

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -8,3 +8,4 @@ export * from './user-type.enum.js';
 export * from './city.enum.js';
 export * from './housing-type.enum.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
